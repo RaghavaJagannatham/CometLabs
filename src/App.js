9a4b7cdc44a198efbad8c9da61388a53d86cdecc
Raghava Jagannatham <raghavajagannatham9@gmail.com>
2023-07-13 10:15:42 +0530

    updated Features and Attributes for the page
    Attributes were updated in the page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import About from './components/About/About';
-import Benefit from './components/About/Benefits/benefits';
+import Benefit from './components/Benefits/benefits';
 import data from './Data/data';
+import Attribute from './components/Attributes/Attribute';
+import features from './Data/features';
+import { Grid } from '@mui/material';
 
 function App() {
   return (
@@ -13,6 +16,19 @@ function App() {
             return <Benefit props={data} />;
           })}
         </div>
+        {/* Attribiutes of the page Section */}
+        <Grid
+        container
+        gap={6}
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        {/* Took data from the features.js that have data of features */}
+        {features.map((data) => {
+          return <Attribute props={data} />;
+        })}
+      </Grid>
     </div>
   );
 }
