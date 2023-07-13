8c080c78534ea7da0cd3e38e65a66ed765764c76
Raghava Jagannatham <raghavajagannatham9@gmail.com>
2023-07-13 09:46:42 +0530

    Updated Benefits in the page
    Benefits folder updated in App.js from the images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import About from './components/About/About';
+import Benefit from './components/About/Benefits/benefits';
+import data from './Data/data';
 
 function App() {
   return (
     <div className="App">
         <About />
+        {/* Benefits has some icons that were taken from data.js file from Data folder */}
+        <div className="benefit-div">
+          {data.map((data) => {
+            return <Benefit props={data} />;
+          })}
+        </div>
     </div>
   );
 }
---
+++ src/components/About/Benefits/benefits.jsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+const Benefit = ({ props }) => {
+  return (
+    <div className="container-benefit">
+      <img src={props.img} alt="icon" />
+      <div>
+        <h2 className="benefit-heading">{props.heading}</h2>
+        <p className="description">{props.description}</p>
+      </div>
+    </div>
+  );
+};
+export default Benefit;
